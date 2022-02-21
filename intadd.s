abfa0234a04f35f7d7a374efb2d4c94fa1728831
RogShotz <lukeroweprivate@gmail.com>
2022-02-21 15:55:51 -0800

    register update

--- intadd.s
+++ intadd.s
@@ -4,20 +4,19 @@
     .global intadd
 
 intadd:
-
-stp x20, x21, [sp, -16]!
-stp x29, x30, [sp, -32]! // probably can remove
+    stp x20, x21, [sp, -16]!
+    stp x29, x30, [sp, -32]! // probably can remove
 
 loop:
 
-eor x21, x0, x1 // xor, for digits left
+    eor x21, x1, x2 // xor, for digits left
 
-mov x20, x0 // stores prev and, which is the result in the final iteration (magic)
-and x1, x0, x1 // and, for carry digits
-lsl x1, x1, #1 // logical shift left to move the carry
+    mov x20, x1 // stores prev and, which is the result in the final iteration (magic)
+    and x2, x1, x2 // and, for carry digits
+    lsl x2, x2, #1 // logical shift left to move the carry
 
-cmp x1, #0 // if there are carries
-bne loop // branch if not equal
+    cmp x2, #0 // if there are carries
+    bne loop // branch if not equal
 
 ldp x20, x21, [sp, 16]
 ldp x29, x30, [sp], 32
