ca6d0fc181be05bc67cd12f722b4cbe65f89da5c
Zack Soucek <zacksoucek@gmail.com>
2022-02-21 17:55:04 -0800

    seubtract works, takes in both numbers in 2-s comp

--- intsub.s
+++ intsub.s
@@ -8,19 +8,22 @@ intsub:
     str x20, [sp, -8]
     stp x29, x30, [sp, -24]
 
-    mov x20, x1 // hold orig val
+    mov x20, x0 // move argument 0 to storage
 
-    mvn x1, x2 // make it two complement
-    mov x2, #1 // adding one to the complement
+    mvn x0, x1 // make it two complement
+    mov x1, #1 // adding one to the complement
 
     bl intadd // add
 
     mov x1, x0 // set intadd result to A
-    mov x2, x20 // retireve orig
+    mov x0, x20 // retireve argumetn 0
 
     bl intadd // add them
 
+    //answer should be correct rn
+
     ldp x20, x21, [sp], 8
     ldp x29, x30, [sp], 24
-ret
+
+    ret
 
