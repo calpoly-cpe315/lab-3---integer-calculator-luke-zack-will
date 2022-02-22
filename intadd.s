2da6a4e176076e17b3cb39355292daa1cd23b774
RogShotz <lukeroweprivate@gmail.com>
2022-02-21 16:38:57 -0800

    hail marry

--- intadd.s
+++ intadd.s
@@ -4,25 +4,26 @@
     .global intadd
 
 intadd:
-    stp x20, x21, [sp, -16]!
-    stp x29, x30, [sp, -32]! // probably can remove
+    str x19, [sp, -8]!
+    stp x20, x21, [sp, -24]!
+    stp x29, x30, [sp, -40]! // probably can remove
 
-    mov x20, x1 // restores primary val
+    mov x19, x1 // restores A
+    mov x20, x2 // restores B
 
 loop:
 
     eor x21, x1, x2 // xor, for digits left
 
-    and x2, x1, x2 // and, for carry digits
+    and x1, x1, x2 // and, for carry digits
     lsl x2, x2, #1 // logical shift left to move the carry 
-
-    mov x1, x21 // stores prev, gives eor and and updated vals for carry
 
     cmp x2, #0 // if there are carries
     bne loop // branch if not equal
 
-mov x0, x1 // give output reg result
-mov x1, x20 // restore for other fncs
+mov x0, x21 // give output reg result
+mov x1, x19 // restore for other fncs
+mov x2, x20
 
 ldp x20, x21, [sp, 16]
 ldp x29, x30, [sp], 32
